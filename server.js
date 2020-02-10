f1c28f52009498978fcf4717e95ab905ca1fb2e9
sanka9686 <sankadilshan9686@gmail.com>
2020-02-10 12:38:53 +0530

    deploy heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist'));
+
+// default Heroku PORT
+app.listen(process.env.PORT || 8080);
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/onlineWorkers-client/index.html'));
+});
+
+console.log('console listening');
+
